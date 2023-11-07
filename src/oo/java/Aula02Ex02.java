ac8f3935e171faa8a14f4e358a01ff3582d92c65
DarcioSouza <darciorsouza@gmail.com>
2023-11-07 11:23:59 -0300

    SENAI Exercicio float em JAVA

---
+++ src/oo/java/Aula02Ex02.java
@@ -0,0 +1,9 @@
+package oo.java;
+
+public class Aula02Ex02 {
+    public static void main(String[] args) {
+        int x=7, y=2;
+        float resultado = (float)x/y;
+        System.out.println("Resultado: " + resultado);
+    }
+}
